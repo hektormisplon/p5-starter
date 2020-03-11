a4e526c2087d976056552c7672640c908da146c9
Hektor Misplon <hektor.misplon@gmail.com>
2020-03-11 21:16:39 +0000

    Init sketch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import './styles/main.css'
 import p5 from 'p5'
 
+const s = p5 => {
+  p5.setup = () => {
+    let canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight)
+  }
+
+  p5.draw = () => {
+    p5.background('#111')
+  }
+}
+
 new p5(s)
